56c65a10b917b429454bbadbcceb2eebbbf691fe
Yash20012004 <157821385+Yash20012004@users.noreply.github.com>
2025-07-14 17:04:27 +0530

    Update monday.java

--- Leetcode Daily/monday.java
+++ Leetcode Daily/monday.java
@@ -1,1 +1,19 @@
-
+  //1290. Convert Binary Number in a Linked List to Integer
+  // 1--0--1   1*(2^0)+0*(2^1)+1*(2^2) 
+  // Tc O(n)+O(n)==O(n)
+    public int getDecimalValue(ListNode head) {
+        int len=-1;
+        ListNode temp=head;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
+        }
+        int num=0;
+        temp=head;
+        while(temp!=null){
+            num+=temp.val*(Math.pow(2,len));
+            len--;
+            temp=temp.next;
+        }
+        return num;
+    }
