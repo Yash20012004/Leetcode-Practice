14576d101e997a9d92ae1f9ba09b25f8b2a3cdaa
Yash20012004 <157821385+Yash20012004@users.noreply.github.com>
2025-07-15 15:58:57 +0530

    Update tuesday.java

--- Leetcode Daily/tuesday.java
+++ Leetcode Daily/tuesday.java
@@ -1,1 +1,16 @@
+3136. Valid Word
 
+   public boolean isValid(String word) {
+        if(word.length()<3) return false;
+        boolean containsVowel=false;
+        boolean containsConsonant=false;
+        for(char ch:word.toCharArray()){
+            if(!Character.isLetterOrDigit(ch)) return false;
+            if(Character.isLetter(ch)){
+                  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+                  containsVowel=true;
+                  else containsConsonant=true;
+            }
+        }
+        return containsVowel&&containsConsonant;
+    }
